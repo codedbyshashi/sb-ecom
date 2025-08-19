89878c782017bef5b14475cb0e96820194bb3a44
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-19 16:57:27 +0530

    implemented interface int the service layer

---
+++ src/main/java/com/ecommerce/Project/service/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ecommerce.Project.service;
+
+import com.ecommerce.Project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+
+    private List<Category> categories = new ArrayList<>();
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categories.add(category);
+
+    }
+
+
+
+}
