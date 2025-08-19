66cedc19b6418c676ed570db8a761f662c52c0a3
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-19 17:15:02 +0530

    Adding to generate id's by the application

--- src/main/java/com/ecommerce/Project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/Project/service/CategoryServiceImpl.java
@@ -10,13 +10,16 @@ public class CategoryServiceImpl implements CategoryService {
 
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
     }
 
+
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
 
     }
