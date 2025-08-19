7c0ed77871f5aee5c117765f7b8068053ccf5f60
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-19 16:56:55 +0530

    Added Business logic in an interface

---
+++ src/main/java/com/ecommerce/Project/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.Project.service;
+
+import com.ecommerce.Project.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+
+}
