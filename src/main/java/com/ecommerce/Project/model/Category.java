59fcca9ba49bc2a5b775cc7dee602d080c655cfc
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-21 17:11:12 +0530

    Added lombok annotations of data,noargsconstructor,allargsconstructor

--- src/main/java/com/ecommerce/Project/model/Category.java
+++ src/main/java/com/ecommerce/Project/model/Category.java
@@ -5,9 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonPropertyOrder({ "categoryId", "categoryName" })
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,22 +25,5 @@ public class Category {
         this.categoryId = categoryId;
     }
 
-    public Category() {
-    }
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
