9af819eaaacf8fea0e12ec5ef229c551bbb691c3
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-21 17:55:12 +0530

    Added ExceptionalHandler

--- src/main/java/com/ecommerce/Project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/Project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.Project.controller;
 
 import com.ecommerce.Project.model.Category;
 import com.ecommerce.Project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class CategoryController {
         return new ResponseEntity<>(categories,HttpStatus.OK);
     }
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
     }
