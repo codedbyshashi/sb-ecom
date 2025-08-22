18c8c858102381bc6ae8e2fa14682e0ab9cec6c5
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-22 18:11:39 +0530

    Added API exception in the globalExceptionHandler

--- src/main/java/com/ecommerce/Project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/Project/exceptions/MyGlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.ecommerce.Project.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,22 +12,27 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class MyGlobalExceptionHandler {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> myMethodArgumentNotValid(MethodArgumentNotValidException e){
         Map<String,String> response = new HashMap<>();
-        e.getBindingResult().getAllErrors().forEach(err ->{
-            String fieldName = ((FieldError)err).getField();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
             String message = err.getDefaultMessage();
-            response.put(fieldName,message);
+            response.put(fieldName, message);
         });
-        return new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
-
-
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public  ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
-
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
-}
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> handleAPIException(APIException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+}   // ✅ closes the class properly
