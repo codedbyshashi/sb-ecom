1b8c3f965eb579b74a0463782dd75830452e424b
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-08-30 23:49:46 +0530

    Added controller for the product

---
+++ src/main/java/com/ecommerce/Project/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.Project.controller;
+
+
+import com.ecommerce.Project.model.Product;
+import com.ecommerce.Project.payload.ProductDto;
+import com.ecommerce.Project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDto> addProduct(@RequestBody Product product,
+                                                 @PathVariable Long categoryId){
+        ProductDto productDto = productService.addProduct(categoryId,product);
+        return new ResponseEntity<>(productDto, HttpStatus.CREATED);
+    }
+}
