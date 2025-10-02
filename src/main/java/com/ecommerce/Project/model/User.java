cfe5cfacbb91415b08dc17b4b9018927b97eeebc
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-10-02 18:35:08 +0530

    Added relationships between user and roles and also implemented lombok

--- src/main/java/com/ecommerce/Project/model/User.java
+++ src/main/java/com/ecommerce/Project/model/User.java
@@ -4,9 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -30,5 +31,21 @@ public class User {
     @NotBlank
     @Size(max=120)
     private String password;
+
+    public User(Long userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE},
+    fetch=FetchType.EAGER)
+    @JoinTable(name="user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name="role_id"))
+    private Set<Role> roles = new HashSet<Role>();
 
 }
