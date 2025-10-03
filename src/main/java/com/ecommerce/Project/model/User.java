53cd2b1aea0ee07f6e15437eac2b18a3afb6a8fc
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-10-03 19:37:43 +0530

    Added UniqueConstraints attribute

--- src/main/java/com/ecommerce/Project/model/User.java
+++ src/main/java/com/ecommerce/Project/model/User.java
@@ -13,23 +13,31 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        })
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max=20)
+    @Column(name="username")
     private String username;
 
     @NotBlank
     @Size(max=20)
     @Email
+    @Column(name="email")
     private String email;
 
     @NotBlank
     @Size(max=120)
+    @Column(name = "password")
     private String password;
 
     public User(Long userId, String username, String email, String password) {
@@ -46,6 +54,6 @@ public class User {
     @JoinTable(name="user_role",
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name="role_id"))
-    private Set<Role> roles = new HashSet<Role>();
+    private Set<Role> roles = new HashSet<>();
 
 }
