8748d9dbcf829c12a2a719b8e1119bc5be61f002
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-10-11 11:29:59 +0530

    Added signin endpoint in the authController

---
+++ src/main/java/com/ecommerce/Project/controller/AuthController.java
@@ -0,0 +1,62 @@
+package com.ecommerce.Project.controller;
+
+import com.ecommerce.Project.security.Request.LoginRequest;
+import com.ecommerce.Project.security.Response.UserInfoResponse;
+import com.ecommerce.Project.security.jwt.JwtUtils;
+import com.ecommerce.Project.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try{
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    )
+            );
+        }catch(AuthenticationException exception){
+            Map<String,Object> map = new HashMap<>();
+            map.put("message","Authentication Failed");
+            map.put("status",false);
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item->item.getAuthority())
+                .collect(Collectors.toList());
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(),userDetails.getUsername(),
+                jwtToken,roles);
+
+        return ResponseEntity.ok(response);
+
+    }
+}
