637e425952fda7b725ec65c57b5d47005cf49411
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-10-16 18:50:07 +0530

    Added a PostMethod signout

--- src/main/java/com/ecommerce/Project/controller/AuthController.java
+++ src/main/java/com/ecommerce/Project/controller/AuthController.java
@@ -23,10 +23,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.desktop.AppReopenedEvent;
 import java.util.*;
@@ -75,7 +72,8 @@ public class AuthController {
                 .map(item->item.getAuthority())
                 .collect(Collectors.toList());
         UserInfoResponse response = new UserInfoResponse(
-                userDetails.getId(),   // id// JWT token
+                userDetails.getId(),
+                // id// JWT token
                 userDetails.getUsername(), // username
                 roles                  // roles
         );
@@ -135,5 +133,40 @@ public class AuthController {
         user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @GetMapping("/username")
+    public String currentUserName(Authentication authentication){
+        if(authentication != null){
+            return authentication.getName();
+        }
+        else{
+            return "";
+        }
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetails(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item->item.getAuthority())
+                .collect(Collectors.toList());
+        UserInfoResponse response = new UserInfoResponse(
+                userDetails.getId(),   // id// JWT token
+                userDetails.getUsername(), // username
+                roles                  // roles
+        );
+
+        return ResponseEntity.ok()
+                .body(response);
+    }
+
+
+    @PostMapping("/signout")
+    ResponseEntity<?> signOutUser(){
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
+                cookie.toString()).body(new MessageResponse("Successfully logged out!"));
     }
 }
