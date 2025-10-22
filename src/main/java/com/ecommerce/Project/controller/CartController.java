9517baf28b52a2b6555c619175fb74ea8ef93b08
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-10-22 23:24:32 +0530

    Added a method for to addProductToCart

---
+++ src/main/java/com/ecommerce/Project/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.Project.controller;
+
+import com.ecommerce.Project.payload.CartDto;
+import com.ecommerce.Project.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDto> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDto cartDto = cartService.addProductToCart(productId,quantity);
+        return new ResponseEntity<CartDto>(cartDto, HttpStatus.CREATED);
+
+    }
+}
