f2d99ace4a9b4f608e2e0321aee5eb5e45c16202
shashidharreddy <kotashashidharreddy1@gmail.com>
2025-12-09 00:24:48 +0530

    Added swagger annotations that improves readability

--- src/main/java/com/ecommerce/Project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/Project/controller/CategoryController.java
@@ -4,6 +4,12 @@ import com.ecommerce.Project.config.AppConstants;
 import com.ecommerce.Project.payload.CategoryDto;
 import com.ecommerce.Project.payload.CategoryResponse;
 import com.ecommerce.Project.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    @Tag(name = "Category APIs",description = "APIs for managing products")
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
             @RequestParam(name = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
@@ -24,16 +32,28 @@ public class CategoryController {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize,sortBy,sortOrder);
         return new ResponseEntity<>(categoryResponse,HttpStatus.OK);
     }
+
+    @Tag(name = "Category APIs",description = "APIs for managing products")
+    @Operation(summary = "Create category",
+            description = "API to create a new category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "Category is created successfully"),
+            @ApiResponse(responseCode = "400",description = "Invalid Input",content = @Content),
+            @ApiResponse(responseCode = "500",description = "Internal server error",content = @Content)
+    })
     @PostMapping("/public/categories")
     public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
         CategoryDto savedCategoryDto = categoryService.createCategory(categoryDto);
         return new ResponseEntity<>(savedCategoryDto,HttpStatus.CREATED);
     }
+
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDto> deleteCategory(@PathVariable Long categoryId) {
+    public ResponseEntity<CategoryDto> deleteCategory(@Parameter(description = "ID of the Category that you wish you to delete")
+                                                          @PathVariable Long categoryId) {
         CategoryDto deleteCategory = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(deleteCategory, HttpStatus.OK);
     }
+
     @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Long categoryId) {
 
